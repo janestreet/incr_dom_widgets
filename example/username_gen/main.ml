33eadd9f7d67d1033e469a27056f64eb94cec38a
Xavier Clerc <xclerc@janestreet.com>
2018-10-25 09:53:23 +0100

    v0.11.120.07+98

--- example/username_gen/main.ml
+++ example/username_gen/main.ml
@@ -1,4 +1,4 @@
 open Incr_dom
 
 let () =
-  Start_app.component (module App) ~initial_model:(App.initial_model ())
+  Start_app.component_old_do_not_use (module App) ~initial_model:(App.initial_model ())
