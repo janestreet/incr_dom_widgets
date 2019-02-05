cac57f494cedeafc2151d5d6462b59d8ed33124d
Xavier Clerc <xclerc@janestreet.com>
2019-02-05 10:09:03 +0000

    v0.12-preview.120.22+10

--- src/incr_dom_widgets.ml
+++ src/incr_dom_widgets.ml
@@ -1,11 +1,6 @@
 
 module Css_gen                         = Css_gen
-module Grouped_help_text               = Grouped_help_text
-module Help_text                       = Help_text
 module Interactive                     = Interactive
-module Keyboard_event                  = Keyboard_event
-module Keyboard_event_handler          = Keyboard_event_handler
-module Keystroke                       = Keystroke
 module Form                            = Form
 module Partial_render_list             = Partial_render_list
 module Row_node_spec                   = Row_node_spec
@@ -14,4 +9,3 @@ module Table                           = Table
 module Table_id                        = Table_id
 module Table_intf                      = Table_intf
 module Util                            = Util
-module Variable_keyboard_event_handler = Variable_keyboard_event_handler
